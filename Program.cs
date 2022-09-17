96fac45b1643f1c78b6f7a998528fd622bda5599
SpiridonowaViky <110791518+SpiridonowaViky@users.noreply.github.com>
2022-09-17 13:42:49 +0300

    new

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+NaturalToLow(numberM, numberN);
+
+
+void NaturalToLow(int n, int m)
+{
+    if (m > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(n, m + 1);
+        Console.Write(m + " ");
+    }
+}
+
